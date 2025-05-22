b08db3789310e7e8c8b094fb511582d8cd7f60c9
KeinerTorres <keiner.torres1@udea.edu.co>
2025-05-21 22:02:45 -0500

    Create reservacion.h

---
+++ reservacion.h
@@ -0,0 +1,43 @@
+#ifndef RESERVACION_H
+#define RESERVACION_H
+#include <string>
+#include "Fecha.h"
+using namespace std;
+
+class Reservacion
+{
+private:
+    string codigo;
+    string codigoAlojamiento;
+    string documentoHuesped;
+    string metodoPago;
+    Fecha fechaEntrada;
+    int duracionNoche;
+    Fecha fechaPago;
+    string anotaciones;
+    int valorTotal;
+    bool activa;
+public:
+    //Constructores
+    Reservacion(const string& cod, const string& codigoAloja, const string& docHues,
+                const string& metPago, Fecha entrada, int duracionNoche, Fecha pago, int valortotal,
+                const string& anot = "");
+
+    //Getters
+    string getCodigo() const;
+    Fecha getFechaEntrada() const;
+    Fecha getFechaSalida() const;
+    bool EstaActiva() const;
+    string getAnotaciones() const;
+
+    //Setters
+    void setActiva(bool estado);
+    void setAnotaciones(const string& notas);
+
+    //Metodos de utilidad
+    void mostrarComprobante() const;
+    void anular();
+
+};
+
+#endif // RESERVACION_H
