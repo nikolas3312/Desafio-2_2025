e10930d24de74453e20228e29bc55dc219fb77e6
KeinerTorres <keiner.torres1@udea.edu.co>
2025-05-22 00:38:28 -0500

    Create anfitrion.h

---
+++ anfitrion.h
@@ -0,0 +1,35 @@
+#ifndef ANFITRION_H
+#define ANFITRION_H
+#include <string>
+using namespace std;
+
+class Anfitrion
+{
+private:
+    string documento;
+    int antiguedadMeses;
+    float puntuacion;
+    string* codigosAlojamiento;
+    int cantidad;
+    int capacidad;
+public:
+    //constructor
+    Anfitrion(const string& doc, int antig, float punt = 0.0);
+    //Destructor
+    ~Anfitrion();
+
+    //Getters
+    string getDocumento() const;
+    float getPuntuacion() const;
+    //Setters
+    void setPuntuacion(float nueva);
+
+    //agregar codigo de alojamiento
+    void agregarCodigoAlojamiento(const string& codigoAlo);
+
+    //informacion del anfitrion
+    void mostrarDetalles() const;
+
+};
+
+#endif // ANFITRION_H
