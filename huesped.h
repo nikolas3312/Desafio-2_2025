f23b271bfde23817bce1cc0cca05cf9b1d54a903
KeinerTorres <keiner.torres1@udea.edu.co>
2025-05-22 22:13:35 -0500

    Create huesped.h

---
+++ huesped.h
@@ -0,0 +1,36 @@
+#ifndef HUESPED_H
+#define HUESPED_H
+#include <string>
+using namespace std;
+
+class Huesped
+{
+private:
+    string id;
+    string nombre;
+    string credencialLogin;
+    string documento;
+    int antiguedadMeses;
+    float puntuacion;
+    string* codigosDeSusReservaciones;
+    int cantidad;
+    int capacidad;
+public:
+    Huesped(const string& doc, int antig, float punt = 0.0);
+    Huesped(const string& id_, const string& nom_, const string& doc_,
+            const string& clave, int antig, float punt);
+    ~Huesped();
+
+    //Getters
+    float getPuntuacion() const;
+
+    //Setters
+    void setPuntuacion(float nueva);
+
+    //Metodos de utilidad
+    void agregarCodigoReservacion(const string& codigoRes);
+    void eliminarCodigoReservacion(const string& codigoRes);
+    void mostrarDetalles() const;
+};
+
+#endif // HUESPED_H
