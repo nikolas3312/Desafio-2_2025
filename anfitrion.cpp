7245990acc3b97cbfb7ccffff769a4401a0313ad
KeinerTorres <keiner.torres1@udea.edu.co>
2025-05-22 22:12:56 -0500

    Update anfitrion.cpp

--- anfitrion.cpp
+++ anfitrion.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 
-Anfitrion::Anfitrion(const string& doc, int antig, float punt)
-    : documento(doc), antiguedadMeses(antig), puntuacion(punt){
+Anfitrion::Anfitrion(const string& id_, const string& nom_,  const string& doc, const string& clave, int antig, float punt)
+    : id(id_), nombre(nom_), documento(doc), contraseñaLogin(clave), antiguedadMeses(antig), puntuacion(punt){
     capacidad = 2;
     cantidad = 0;
     codigosAlojamiento = new string[capacidad];
@@ -35,7 +35,10 @@ void Anfitrion::agregarCodigoAlojamiento(const string &codigoAlo) {
 }
 
 void Anfitrion::mostrarDetalles() const {
-    cout << "Anfitrion: " << documento << endl;
+    cout << "ID: " << id << endl;
+    cout << "Nombre completo: " << nombre << endl;
+    cout << "Documento: " << documento << endl;
+    cout << "Contraseña login" << contraseñaLogin << endl;
     cout << "antiguedad (meses): " << antiguedadMeses << endl;
     cout << "Puntuacion: " << puntuacion << endl;
     cout << "Alojamiento administrados (" << cantidad << "): " << endl;
