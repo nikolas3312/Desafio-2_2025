49c94c9d6566fc28e0b46ea822ccd0cbc19b331f
KeinerTorres <keiner.torres1@udea.edu.co>
2025-05-25 22:54:58 -0500

    Update anfitrion.cpp

--- anfitrion.cpp
+++ anfitrion.cpp
@@ -1,9 +1,47 @@
 #include "anfitrion.h"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 Anfitrion::Anfitrion(const string& id_, const string& nom_,  const string& doc, const string& clave, int antig, float punt)
     : id(id_), nombre(nom_), documento(doc), contraseñaLogin(clave), antiguedadMeses(antig), puntuacion(punt){
+    capacidad = 2;
+    cantidad = 0;
+    codigosAlojamiento = new string[capacidad];
+}
+
+Anfitrion& Anfitrion::operator=(const Anfitrion& otro) {
+    if (this != &otro) {
+        // Liberar memoria previa
+        delete[] codigosAlojamiento;
+
+        // Copiar los datos
+        id = otro.id;
+        nombre = otro.nombre;
+        documento = otro.documento;
+        contraseñaLogin = otro.contraseñaLogin;
+        antiguedadMeses = otro.antiguedadMeses;
+        puntuacion = otro.puntuacion;
+        cantidad = otro.cantidad;
+        capacidad = otro.capacidad;
+
+        codigosAlojamiento = new string[capacidad];
+        for (int i = 0; i < cantidad; ++i) {
+            codigosAlojamiento[i] = otro.codigosAlojamiento[i];
+        }
+    }
+    return *this;
+}
+
+
+Anfitrion::Anfitrion() {
+    id = "";
+    nombre = "";
+    documento = "";
+    contraseñaLogin = "";
+    antiguedadMeses = 0;
+    puntuacion = 0.0;
     capacidad = 2;
     cantidad = 0;
     codigosAlojamiento = new string[capacidad];
@@ -38,11 +76,22 @@ void Anfitrion::mostrarDetalles() const {
     cout << "ID: " << id << endl;
     cout << "Nombre completo: " << nombre << endl;
     cout << "Documento: " << documento << endl;
-    cout << "Contraseña login" << contraseñaLogin << endl;
+    cout << "Contrasena login" << contraseñaLogin << endl;
     cout << "antiguedad (meses): " << antiguedadMeses << endl;
     cout << "Puntuacion: " << puntuacion << endl;
     cout << "Alojamiento administrados (" << cantidad << "): " << endl;
     for (int i = 0; i < cantidad; ++i) {
         cout << " - " << codigosAlojamiento[i] << endl;
     }
+}
+
+string Anfitrion::toFileString() const {
+    ostringstream oss;
+    oss << id << ","
+        << "\"" << nombre << "\","
+        << documento << ","
+        << "\"" << contraseñaLogin << "\","
+        << antiguedadMeses << ","
+        << fixed << setprecision(1) << puntuacion;
+    return oss.str();
 }
